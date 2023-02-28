b412db1e17a3082f2dd7ad47d9aebbb8dd5877fd
Kenneth Lu <kian901@gmail.com>
2023-02-28 19:14:49 +0800

    Session 15 Activity

---
+++ index.js
@@ -0,0 +1,72 @@
+let firstName = "Kenneth Ian";
+let lastName = ' Lu';
+// console.log(firstName);
+// console.log(lastName);
+
+let fullName = firstName + lastName;
+// console.log(fullName);
+
+
+
+let Name1 = "First Name: " + firstName;
+console.log(Name1);
+
+let Name2 = "Last Name: " + lastName;
+console.log(Name2);
+
+let Age1 = 17;
+// console.log(Age1);
+
+let Age2 = "Age: " + Age1;
+console.log(Age2);
+
+let Hobbie = "Hobbies: ";
+console.log(Hobbie);
+
+const Hobbies = [ "Playing Video Games", "Watching Anime"," Drawing", "Coding" ];
+console.log(Hobbies);
+
+let workAdd = "Work Address: ";
+console.log(workAdd);
+
+let Address = {
+	houseNumber: "8",
+	street: "Argao St",
+	city: "Manila",
+	state:"N/A"
+};
+console.log(Address);
+
+let Name = "My full name is: " + fullName;
+console.log(Name);
+
+let currentAge = " My current age is: " + Age1;
+console.log(currentAge)
+
+let Friend = "My Friends are: ";
+console.log(Friend);
+
+const Friends = [ "Mark", "Harold", "Elijah", "Asis", "Alex", "Ryu", "Seth" ];
+console.log(Friends);
+
+let Profile = "My Full Profile: ";
+console.log(Profile);
+
+let profileDetails = {
+	username: "Chaotic",
+	fullname: "Kenneth Ian Lu",
+	Age: "17",
+	isActive: true,
+};
+console.log(profileDetails);
+
+let bff = " Kahrl Asis"
+
+let bestFriend = "My Best Friend is:" + bff ;
+console.log(bestFriend);
+
+let place = " Tokyo, Japan"
+
+
+let Place = "I wanna go to:" + place ;
+console.log(Place);
